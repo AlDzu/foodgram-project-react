be1fb726f7353ea3ba29e3aeaa9edf72c730245e
Александр Дзюба <komrad.dzyuba2014@yandex.ru>
2023-02-26 18:02:35 +0300

    ревью 2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,9 +1,12 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv("SECRET_KEY", default="DefaultKey")
+SECRET_KEY = os.getenv("SECRET_KEY", "DefaultKey")
 
 DEBUG = True
 
