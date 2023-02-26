98fca3df40693adb28f6b26588e55ecb749e9d22
Александр Дзюба <komrad.dzyuba2014@yandex.ru>
2023-02-26 04:04:33 +0300

    ревью 2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,9 @@ from rest_framework import routers
 
 from . import views
 
+
+app_name = 'api'
+
 router_v1 = routers.DefaultRouter()
 router_v1.register(
     "recipes",
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -61,8 +61,6 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("POSTGRES_USER"),
@@ -125,7 +123,8 @@ DJOSER = {
     },
 }
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
